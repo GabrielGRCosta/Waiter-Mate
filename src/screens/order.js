37a8c3bd9c13bb1c012acf5c080999403425ee2e
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2024-07-05 19:29:48 -0300

    WIP update order, refact bill

--- src/screens/order.js
+++ src/screens/order.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Divider, List, ListItem, Icon, TopNavigation, TopNavigationAction, Layout, Button, useTheme } from '@ui-kitten/components';
+import { Divider, List, ListItem, Card, Text, Icon, TopNavigation, TopNavigationAction, Layout, Button } from '@ui-kitten/components';
 
-const items = [
-    //{ name: '1 Pizza Mozzarella G'   },
-    //{ name: '1 Pizza Pepperoni G'    },
-    //{ name: '1 Pizza Pepperoni G'    },
-    //{ name: '1/2 Pizza Margherita G' },
-    //{ name: '1/2 Pizza Portuguese G' },
-    //{ name: '1 Pizza Chicken G'      },
-    //{ name: '1/4 Pizza Chocolate G'  },
-    //{ name: '1/4 Pizza White Chocolate G'  },
-    //{ name: '1/4 Pizza Pineapple G'  },
-    //{ name: '1/4 Pizza Vanilla Icecream G' },
-].map((e, i) => ({ id: i+1, ...e }));
+export const OrderScreen = ({ navigation, route }) => {
+  const { order } = route.params;
 
-export const OrderScreen = ({ navigation }) => {
-  const theme = useTheme();
-
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-      <ListItem
-        title={`${item.id}#--  ${item.name}`}
-        description={item.observation || ''}
-        accessoryLeft={
-          <Icon name='clock-outline' />
-        }
-        style={{ backgroundColor: theme['background-basic-color-2']}}
+  const renderOrder = ({ item: order }) => (
+    <Card
+      status={order.status === 'served' ? 'success' : order.status == 'waiting' ? 'warning' : 'danger' }
+      header={<Text>Pedido #{order.ord}</Text>}
+      style={{ marginBottom: 5 }}
+    >
+      <List
+        data={order.items}
+        renderItem={({ index, item }) => (
+          <ListItem
+            title={`${item.name}`}
+            description={item.observation || ''}
+            accessoryLeft={
+              <Icon name='chevron-right-outline' />
+            }
+            accessoryRight={
+              <Icon name='trash' />
+            }
+          />
+        )}
+        keyExtractor={item => item.id}
       />
-    </TouchableOpacity>
+    </Card>
   );
 
   return (
@@ -49,26 +48,23 @@ export const OrderScreen = ({ navigation }) => {
       <Divider />
       <Layout style={{ padding: 15, flex: 1 }}>
         <List
-          data={items}
-          renderItem={renderItem}
-          keyExtractor={item => item.id.toString()}
-          ItemSeparatorComponent={Divider}
+          data={[ order ]}
+          renderItem={renderOrder}
+          keyExtractor={order => order.id}
           style={{ padding: 10 }}
         />
       </Layout>
       <Layout style={{ padding: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
         <Button
           status='danger'
-          styles={{ flex: 3 }}
           accessoryLeft={
-            <Icon name='trash-2-outline' />
+            <Icon name='minus-circle-outline' />
           }
         >
-          Encerrar
+          Cancelar
         </Button>
         <Button
           status='warning'
-          styles={{ flex: 3 }}
           accessoryLeft={
             <Icon name='bell-outline' />
           }
@@ -79,7 +75,6 @@ export const OrderScreen = ({ navigation }) => {
           accessoryLeft={
             <Icon name='plus' />
           }
-          styles={{ flex: 1 }}
         />
       </Layout>
     </Layout>
